ec4d403da4b344fcf8cf68d92ed060114ef11cb2
Muhammad Abu Bakar <145241625+abubakar51@users.noreply.github.com>
2025-06-05 06:22:39 +0600

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: isSecure, // true for HTTPS/devtunnel, false for localhost
-      sameSite: "none", // Cross-site এর জন্য
+      sameSite: "lax", // Cross-site এর জন্য
       maxAge: 10 * 60 * 1000,
     },
   })
